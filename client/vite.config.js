c14b6faf8007c399cd7cb7550a4c6138b784ffa2
Maria Babar <mariababar12340@gmail.com>
2026-09-14 13:01:52 +0500

    Add legacy browser support

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    legacy({
+      targets: [
+        "Chrome >= 60",
+        "Android >= 8",
+        "iOS >= 12",
+        "Safari >= 12"
+      ],
+      modernPolyfills: true,
+      renderLegacyChunks: true
+    })
+  ],
 
   build: {
-    target: "es2015",
+    target: "es2015"
   },
 
   esbuild: {
-    target: "es2015",
-  },
+    target: "es2015"
+  }
 });
